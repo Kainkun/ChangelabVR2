2e6faeb770ede02851a79816e609076bb4392dea
Ernesto Portillo <106776594+hihidude2@users.noreply.github.com>
2022-06-23 16:23:56 -0700

    Refined the pop up with material change + misc improvements

--- Assets/Scripts/TextPopup.cs
+++ Assets/Scripts/TextPopup.cs
@@ -6,33 +6,39 @@ public class TextPopup : MonoBehaviour
 {
     public bool isInTrigger = false;
     public bool isLookingAtObject = false;
+    public GameObject importantThingo;
+    public Material highlightMaterial;
+    public Material originalMaterial;
     public GameObject textPanel;
+    public Text text;
     // Start is called before the first frame update
     void Start()
     {
-        
+        originalMaterial = importantThingo.GetComponent<MeshRenderer>().material;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (isInTrigger)
+        if (isInTrigger && isLookingAtObject)
         {
-            if (isLookingAtObject)
-            {
-                textPanel.SetActive(true);
-            }
+            textPanel.SetActive(true);
+            text.text = "This Cube is an Ancient Handball before they invented rounded corners.";
+            text.fontSize = 219;
+            importantThingo.GetComponent<MeshRenderer>().material = highlightMaterial;
         }
         else
         {
             textPanel.SetActive(false);
+            importantThingo.GetComponent<MeshRenderer>().material = originalMaterial;
         }
     }
 
     private void OnTriggerStay(Collider other)
     {
+        //DISABLE OTHER COLLIDERS ON THE TELEPORT PADS, MESSING WITH THE TRIGGER ZONE
         print("collision happened");
-        if(other.gameObject.tag == "InteractableTrigger")
+        if (other.gameObject.tag == "InteractableTrigger")
         {
             isInTrigger = true;
             print("entered");
@@ -42,5 +48,16 @@ public class TextPopup : MonoBehaviour
             isInTrigger = false;
         }
     }
-    
+
+    public void ObjectLookedAt()
+    {
+
+        isLookingAtObject = true;
+    }
+    public void ObjectNotLookedAt()
+    {
+
+        isLookingAtObject = false;
+    }
+
 }
